3e7d5d0edb3858a211602789c8b8e7b2567fd03c
SteliosGian <steliosgiannik@gmail.com>
2023-05-12 13:19:10 +0300

    Add run script

--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
+
+MODE=${1:-inference}
+
 set -euo pipefail
 IFS=$'\n\t'
 trap "exit 1" HUP INT QUIT ABRT SEGV
 
 docker build --pull --tag fake-news-detector:local .
-docker run --user root --rm -it fake-news-detector:local
+
+if [[ $MODE == "inference" ]]
+then 
+    docker run --user root --rm -it -p 8080:8080 fake-news-detector:local
+elif [[ $MODE == "training" ]] 
+then
+    docker run --user root --rm -it -p 8080:8080 --mount type=bind,source=./src/inference/temp_model,target=/app/inference/temp_model fake-news-detector:local python fake_news_detector/train.py
+else
+    echo "Invalid argument. Only inference/training are allowed."
+fi
